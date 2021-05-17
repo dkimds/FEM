a848347375b5a5780ff01b168e032d30f1f4dd47
Daeseong <dkimds@gmail.com>
2021-05-17 14:29:22 +0900

    Update bfem210517.m
    Merge the constructions of stiffness matrix and RHS

--- bfem210517.m
+++ bfem210517.m
@@ -10,8 +10,8 @@ xw=[0.33333333333333    0.33333333333333   -0.56250000000000
     0.20000000000000    0.60000000000000    0.52083333333333];
 
 % Generate mesh
-h=1/8;
-x=0:h:1;
+h = 1/8;
+x = 0:h:1;
 M = length(x);
 col = repmat(x',M,1);
 row = reshape(repmat(x,M,1),[numel(col),1]);
@@ -27,7 +27,7 @@ for j = 1:M-1
     end
 end
 
-%stiffness matrix A
+%stiffness matrix A and RHS b
 A =sparse(M^2,M^2);
 b=zeros(length(N),1);
 
@@ -40,30 +40,32 @@ for n = 1:length(T)
     p1 = m\([1 0 0]');
     p2 = m\([0 1 0]');
     p3 = m\([0 0 1]');
-    
+    % Area of a triangle
+    S = abs(det(m))*0.5; 
+
     % Contruct stiff matrix
     % element stiff matrix
-    p11=p1(1:2)'*p1(1:2)*abs(det(m))*0.5;
-    p22=p2(1:2)'*p2(1:2)*abs(det(m))*0.5;
-    p33=p3(1:2)'*p3(1:2)*abs(det(m))*0.5;
-    p12=p1(1:2)'*p2(1:2)*abs(det(m))*0.5;
-    p23=p2(1:2)'*p3(1:2)*abs(det(m))*0.5;
-    p13=p1(1:2)'*p3(1:2)*abs(det(m))*0.5;
-    p21=p12;
-    p32=p23;
-    p31=p13;
+    p11 = p1(1:2)'*p1(1:2)*S;
+    p22 = p2(1:2)'*p2(1:2)*S;
+    p33 = p3(1:2)'*p3(1:2)*S;
+    p12 = p1(1:2)'*p2(1:2)*S;
+    p23 = p2(1:2)'*p3(1:2)*S;
+    p13 = p1(1:2)'*p3(1:2)*S;
+    p21 = p12;
+    p32 = p23;
+    p31 = p13;
     % Assembly
-    A(k(1),k(1))=A(k(1),k(1))+p11;
-    A(k(2),k(2))=A(k(2),k(2))+p22;
-    A(k(3),k(3))=A(k(3),k(3))+p33;
-    A(k(1),k(2))=A(k(1),k(2))+p12;
-    A(k(2),k(3))=A(k(2),k(3))+p23;
-    A(k(1),k(3))=A(k(1),k(3))+p13;
-    A(k(2),k(1))=A(k(2),k(1))+p21;
-    A(k(3),k(2))=A(k(3),k(2))+p32;
-    A(k(3),k(1))=A(k(3),k(1))+p31;
+    A(k(1),k(1)) = A(k(1),k(1))+p11;
+    A(k(2),k(2)) = A(k(2),k(2))+p22;
+    A(k(3),k(3)) = A(k(3),k(3))+p33;
+    A(k(1),k(2)) = A(k(1),k(2))+p12;
+    A(k(2),k(3)) = A(k(2),k(3))+p23;
+    A(k(1),k(3)) = A(k(1),k(3))+p13;
+    A(k(2),k(1)) = A(k(2),k(1))+p21;
+    A(k(3),k(2)) = A(k(3),k(2))+p32;
+    A(k(3),k(1)) = A(k(3),k(1))+p31;
 
-    % Construct RHS b
+    % Construct RHS
     % Original Triangle
     x1 = m(1,1); 
     y1 = m(1,2);
@@ -71,14 +73,15 @@ for n = 1:length(T)
     y2 = m(2,2);
     x3 = m(3,1); 
     y3 = m(3,2);  
-    S = abs(det(m))*0.5; % Area of a triangle
-    % Integration
+    % Integration w/ Gaussian quadrature on reference triangle
     qd1 =0;
     qd2 =0;
     qd3 =0;
     for i = 1:4
+        % calculate quadrature pts on ref triangle
         s = x1 + (x2-x1)*xw(i,1) + (x3-x1)*xw(i,2);
         t = y1 + (y2-y1)*xw(i,1) + (y3-y1)*xw(i,2);
+        % Gaussian quadrature
         qd1 = qd1 + S*xw(i,3)*f(s,t)*(p1(1)*s + p1(2)*t + p1(3));
         qd2 = qd2 + S*xw(i,3)*f(s,t)*(p2(1)*s + p2(2)*t + p2(3));
         qd3 = qd3 + S*xw(i,3)*f(s,t)*(p3(1)*s + p3(2)*t + p3(3));
@@ -89,7 +92,7 @@ for n = 1:length(T)
     b(k(3)) = b(k(3)) + qd3;  
 end
 
-% Remove basis ftns on boundary
+% Remove basis ftns on bdr
 B = [];
 for k = 1:length(N)
     x = N(k,1);
@@ -146,7 +149,7 @@ while stop_crt > 1e-5
     stop_crt = max(x_new-x)/max(x_new);
 end
 
-% Conjugate Gradient  
+% Conjugate Gradient method
 stop_crt = 1;
 k3 =0;
 x_new = zeros(M,1);
@@ -166,10 +169,8 @@ while stop_crt > 1e-5
     beta = (r_new'*(A*d))/(d'*(A*d));
     d = -r_new + beta*d;
         
-    stop_crt = max(x_new-x)/max(x_new);    
+    stop_crt = max(x_new - x)/max(x_new);    
 end
-
-
 
 
 
