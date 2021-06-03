74cac6cead79c13fa830af0f30841eceb74176d7
Daeseong <dkimds@gmail.com>
2021-06-03 12:47:23 +0900

    Update bfem210315
    Coordinates using for-loop

--- bfem210315.m
+++ bfem210315.m
@@ -1,6 +1,20 @@
 h = 1/10;
 x=0:h:1; M=length(x);
 
+% Double for-loops
+for i =1:length(x)
+    for j = 1:length(x)
+        N(length(x)*(i-1)+j, 1:2)=[(i-1)*h (j-1)*h];
+    end
+end
+plot(N(:,1),N(:,2),".")
+
+% Single for-loops
+for i =1:M
+    N1(M*(i-1)+1: M*i, 1:2)=[(i-1)*h*ones(M,1) x'];
+end
+plot(N1(:,1),N1(:,2),".")
+
 % 2-lines
 col = repmat(x, 1, M);
 row = reshape(repmat(x, M, 1), [numel(repmat(x, M, 1)), 1]);
